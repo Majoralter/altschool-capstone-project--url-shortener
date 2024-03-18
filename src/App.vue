d559ca7b385435583c018be9db636796b2850f2a
Jenrola Jackson <jacksinsthegenius@gmail.com>
2024-03-18 21:37:50 +0100

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <Nav />
   <router-view/>
+  <Footer />
 </template>
 
 <script lang="ts"> 
 import { defineComponent } from 'vue';
 import Nav from './components/Nav.vue';
+import Footer from './components/Footer.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    Nav
+    Nav,
+    Footer
   }
 })
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    🫣 designed && coded by <a href="https://twitter.com/MajorAlter">MajorAlter</a> 🫣
+  </footer>
+</template>
+
+<script>
+export default {
+name: 'Footer'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../sass/app.scss";
+footer{
+  text-align: center;
+  font-size: var(--font-size-3);
+  margin-bottom: var(--size-5);
+}
+</style>
+
